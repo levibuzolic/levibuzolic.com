29ebd5655f59077ad31021cffcf2a37fc396fd5e
Levi Buzolic <levibuzolic@gmail.com>
2018-03-23 15:09:14 +1100

    Fatures

--- gulpfile.js
+++ gulpfile.js
@@ -4,18 +4,30 @@ const imagemin = require('gulp-imagemin');
 const sass = require('gulp-sass');
 const uglify = require('gulp-uglify');
 const {noop} = require('gulp-util');
-const livereload = require('gulp-livereload');
 const cleanCSS = require('gulp-clean-css');
+const connect = require('gulp-connect');
+const ejs = require('gulp-ejs');
+const autoprefixer = require('gulp-autoprefixer');
+const htmlmin = require('gulp-htmlmin');
 
 const DEST = './public/';
 let isProd = false;
+
+gulp.task('html', () => gulp
+  .src('src/index.ejs')
+  .pipe(ejs({}, {}, {ext: '.html'}))
+  .pipe(isProd ? htmlmin({collapseWhitespace: true}) : noop())
+  .pipe(gulp.dest(DEST))
+  .pipe(connect.reload())
+);
 
 gulp.task('css', () => gulp
   .src('src/style.scss')
   .pipe(sass().on('error', sass.logError))
   .pipe(isProd ? cleanCSS() : noop())
+  .pipe(isProd ? autoprefixer() : noop())
   .pipe(gulp.dest(DEST))
-  .pipe(isProd ? noop() : livereload())
+  .pipe(connect.reload())
 );
 
 gulp.task('js', () => gulp
@@ -38,23 +50,32 @@ gulp.task('js', () => gulp
   }))
   .pipe(isProd ? uglify() : noop())
   .pipe(gulp.dest(DEST))
-  .pipe(isProd ? noop() : livereload())
+  .pipe(connect.reload())
 );
 
 gulp.task('img', () => gulp
   .src('src/images/*')
   .pipe(imagemin())
   .pipe(gulp.dest(DEST + 'images/'))
+  .pipe(connect.reload())
+);
+
+gulp.task('fonts', () => gulp
+  .src('src/fonts/*')
+  .pipe(gulp.dest(DEST + 'fonts/'))
+  .pipe(connect.reload())
 );
 
 gulp.task('watch', () => {
-  livereload.listen();
-
   gulp.watch('src/**/*.{css,sass,scss}', ['css']);
   gulp.watch('src/**/*.js', ['js']);
 });
 
+gulp.task('connect', function() {
+  connect.server({root: 'public', livereload: true});
+});
+
 gulp.task('production', () => isProd = true);
 
-gulp.task('build', ['production', 'css', 'js', 'img']);
-gulp.task('default', ['watch', 'css', 'js', 'img']);
+gulp.task('build', ['production', 'html', 'css', 'js', 'img', 'fonts']);
+gulp.task('default', ['watch', 'html', 'css', 'js', 'img', 'fonts', 'connect']);
